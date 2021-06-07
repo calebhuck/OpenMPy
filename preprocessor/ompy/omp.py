fa6f51d2549d2132b5d6c324a2090645728f3eb4
huck0218 <cthuck42@students.tntech.edu>
2021-06-07 12:18:14 -0500

    added omp_get_wtime to runtime

--- preprocessor/ompy/omp.py
+++ preprocessor/ompy/omp.py
@@ -1,4 +1,6 @@
 from threading import current_thread
+from time import time
+
 
 def omp_get_thread_num():
     try:
@@ -6,5 +8,11 @@ def omp_get_thread_num():
     except AttributeError:
         # this only happens when called from the main thread
         return 0
+
+
 def omp_get_num_threads():
     return current_thread().get_num_threads()
+
+
+def omp_get_wtime():
+    return time()
